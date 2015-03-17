44c9a54f7a4b9fe6327ba365eee237ae7377c921
Ken Goydan <Wilkopolis@gmail.com>
2015-03-17 11:49:24 -0400

    Initial Commit

---
+++ src/main/html/src/edu/udel/cisc275_15s/bigo/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package edu.udel.cisc275_15s.bigo.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import edu.udel.cisc275_15s.bigo.BigOGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new BigOGame();
+        }
+}
