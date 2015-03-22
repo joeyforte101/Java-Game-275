ed1914bd45ab0ab61a10b8adbb40f770d7e8d473
aleks <aleks@128.4.124.165>
2015-03-22 14:28:41 -0400

    update test

--- src/main/core/src/edu/udel/cisc275_15s/bigo/BigOGame.java
+++ src/main/core/src/edu/udel/cisc275_15s/bigo/BigOGame.java
@@ -18,6 +18,7 @@ public class BigOGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		//hello this test
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
