c7f570f2d309e718cc29118c5fe9bc4efe7fe9b7
ggraney <ggraney@udel.edu>
2015-04-09 20:11:18 -0400

    This is not a working version yet. Currently in progress

--- src/main/core/src/edu/udel/cisc275_15s/bigo/Splash.java
+++ src/main/core/src/edu/udel/cisc275_15s/bigo/Splash.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -25,11 +26,11 @@ public class Splash implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
-        stage.act();
-        stage.draw();
-        /*batch.begin();
+        //stage.act();
+        //stage.draw();
+        batch.begin();
         batch.draw(animation.getKeyFrame((time += delta), true), 0, 0);
-        batch.end();*/
+        batch.end();
         
     }
 
@@ -39,17 +40,28 @@ public class Splash implements Screen {
 
     @Override
     public void show() {
-        stage.addActor(splashImage);
-    	//batch = new SpriteBatch();
-        //animation = new Animation(1 / 3f, new TextureRegion(new Texture("output_JlccUQ.gif"))sett);
-        //animation.setPlayMode(Animation);
+        //stage.addActor(splashImage);
+    	batch = new SpriteBatch();
+        animation = new Animation(1/18f, new TextureRegion(new Texture("bigOTitle/TheBigO#1.jpg")), new TextureRegion(new Texture("bigOTitle/TheBigO#2.jpg")),
+        		new TextureRegion(new Texture("bigOTitle/TheBigO#3.jpg")),new TextureRegion(new Texture("bigOTitle/TheBigO#4.jpg")),
+        		new TextureRegion(new Texture("bigOTitle/TheBigO#5.jpg")),new TextureRegion(new Texture("bigOTitle/TheBigO#6.jpg")),
+        		new TextureRegion(new Texture("bigOTitle/TheBigO#7.jpg")),new TextureRegion(new Texture("bigOTitle/TheBigO#8.jpg")),
+        		new TextureRegion(new Texture("bigOTitle/TheBigO#9.jpg")),new TextureRegion(new Texture("bigOTitle/TheBigO#10.jpg")),
+        		new TextureRegion(new Texture("bigOTitle/TheBigO#11.jpg")),new TextureRegion(new Texture("bigOTitle/TheBigO#12.jpg")),
+        		new TextureRegion(new Texture("bigOTitle/TheBigO#13.jpg")),new TextureRegion(new Texture("bigOTitle/TheBigO#14.jpg")),
+        		new TextureRegion(new Texture("bigOTitle/TheBigO#15.jpg")),new TextureRegion(new Texture("bigOTitle/TheBigO#16.jpg")),
+        		new TextureRegion(new Texture("bigOTitle/TheBigO#17.jpg")));
+        
+        
+        //((Game)Gdx.app.getApplicationListener()).setScreen(new MainMenu());
+        /*//animation.setPlayMode(Animation);
         splashImage.addAction(Actions.sequence(Actions.alpha(0)
-                       ,Actions.fadeIn(0.5f),Actions.delay(2),Actions.run(new Runnable() {
+                       ,Actions.fadeIn(.5f),Actions.delay(6),Actions.run(new Runnable() {
             @Override
             public void run() {
                 ((Game)Gdx.app.getApplicationListener()).setScreen(new MainMenu());
             }
-        })));
+        })));*/
     }
 
     @Override
@@ -69,6 +81,7 @@ public class Splash implements Screen {
     public void dispose() {
         texture.dispose();
         stage.dispose();
+        
     }
 
 }
