b8fb3253b008661e4823092a8a9edeca5ddc19f1
aleks <aleks@128.4.121.229>
2015-05-05 14:04:25 -0400

    hilarious animation, I'll make it look smoother soon

--- src/main/core/src/gameObjects/Entity/UserCharacter.java
+++ src/main/core/src/gameObjects/Entity/UserCharacter.java
@@ -1,8 +1,11 @@
 package gameObjects.Entity;
 
 import java.util.List;
+
 import helperClasses.Directions;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Intersector;
 
 public class UserCharacter extends Entity {
@@ -10,12 +13,26 @@ public class UserCharacter extends Entity {
 	public final int HEIGHT = Gdx.graphics.getHeight();
 
 	public int moveSpeed = 4;
+	public int animationCounter = 0;
 	public Directions direction;
 	public boolean talking = false;
 	public boolean movingHorizontally = true;
+	private Texture[] upTextures = { new Texture("Game character/up1.png"),
+			new Texture("Game character/up2.png"),
+			new Texture("Game character/up3.png") };
+	private Texture[] rightTextures = {
+			new Texture("Game character/right1.png"),
+			new Texture("Game character/right2.png"),
+			new Texture("Game character/right3.png") };
+	private Texture[] leftTextures = { new Texture("Game character/left1.png"),
+			new Texture("Game character/left2.png"),
+			new Texture("Game character/left3.png") };
+	private Texture[] downTextures = { new Texture("Game character/down1.png"),
+			new Texture("Game character/down2.png"),
+			new Texture("Game character/down3.png") };
 
 	public UserCharacter() {
-		super(0, 0, "playerBack.png");
+		super(0, 0, "Game character/up2.png");
 	}
 
 	public void setTalking(boolean t) {
@@ -23,9 +40,12 @@ public class UserCharacter extends Entity {
 	}
 
 	public void resetPositionAfterIntersection(List<Obstacle> obstacles) {
+		if (animationCounter > 2)
+			animationCounter = 0;
 		for (Obstacle o : obstacles) {
 			switch (direction) {
 			case UP:
+				texture = upTextures[animationCounter];
 				if (Intersector.overlaps(hitBox, o.getHitBox())) {
 					hitBox.y = o.getY() - getHeight() - 1;
 					movingHorizontally = true;
@@ -52,5 +72,20 @@ public class UserCharacter extends Entity {
 				break;
 			}
 		}
+		switch (direction) {
+		case UP:
+			texture = upTextures[animationCounter];
+			break;
+		case DOWN:
+			texture = downTextures[animationCounter];
+			break;
+		case RIGHT:
+			texture = rightTextures[animationCounter];
+			break;
+		case LEFT:
+			texture = leftTextures[animationCounter];
+			break;
+		}
+		animationCounter++;
 	}
 }
