8807def8f1ebaf8bbc8a92e8746fac04e6c200b9
Vin <Vin@VinPC>
2015-05-10 20:30:50 -0400

    bug fix: there are dummy one.qstn in most of the dialog folders.  this is so that all the packages commit. I also did a little messing around with the yesnonpc. HOwever, it is still not perfect

--- src/main/core/src/gameObjects/Entity/YesNoNPC.java
+++ src/main/core/src/gameObjects/Entity/YesNoNPC.java
@@ -11,30 +11,32 @@ public class YesNoNPC extends NPC {
 	
 	String messageYes;
 	String messageNo;
+	String question;
 	int understood;
 
 	public YesNoNPC(YesNoQuestion question, int[] position){
 		super(position[0], position[1], "npc_sprites/trainer.png", question.message, question.notes);
+		this.question = question.question;
 		this.messageYes = question.yes;
-		this.messageNo = question.no;		
+		this.messageNo = question.no;	
 	}
 	
 	public YesNoNPC(int x, int y, String sprite, String message, String messageY, String messageN, String notes){
 		super(x, y, sprite, message, notes);
 		this.messageYes = messageY;
-		this.messageNo = messageN;		
+		this.messageNo = messageN;	
 	}
 
 	public YesNoNPC(int[] position, String message, String messageY, String messageN, String notes){
 		super(position[0], position[1], "npc_sprites/trainer.png", message, notes);
 		this.messageYes = messageY;
-		this.messageNo = messageN;		
+		this.messageNo = messageN;	
 	}
 	
 	public YesNoNPC(int x, int y, String message, String messageY, String messageN, String notes){
 		super(x, y, "npc_sprites/trainer.png", message, notes);
 		this.messageYes = messageY;
-		this.messageNo = messageN;		
+		this.messageNo = messageN;
 	}
 
 	public String getMessageYes() {
@@ -52,6 +54,10 @@ public class YesNoNPC extends NPC {
 	public void setMessageNo(String messageNo) {
 		this.messageNo = messageNo;
 	}
+	
+	public String getQuestion(){
+		return question;
+	}
 
 	public int isYes() {
 		return understood;
@@ -68,7 +74,7 @@ public class YesNoNPC extends NPC {
 		batch.draw(textbox, 20, 60);
 		
 		switch(this.understood){
-		case 0: text.draw(batch, getMessage(), 30, 60 + textbox.getHeight() - 10); break;
+		case 0: text.draw(batch, getQuestion(), 30, 60 + textbox.getHeight() - 10); break;
 		case 1: text.draw(batch, getMessageYes(), 30, 60 + textbox.getHeight() - 10); break;
 		case 2: text.draw(batch, getMessageNo(), 30, 60 + textbox.getHeight() - 10); break;
 		}
